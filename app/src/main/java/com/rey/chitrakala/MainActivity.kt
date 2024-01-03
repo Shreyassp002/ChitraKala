3fea394e8853fe4f04fe5355552c0d2053e09ce5
Shreyas Patil <shreyassp002@gmail.com>
2024-01-03 23:49:54 +0530

    Added Provider, path and Image sandwich maker

--- app/src/main/java/com/rey/chitrakala/MainActivity.kt
+++ app/src/main/java/com/rey/chitrakala/MainActivity.kt
@@ -4,6 +4,9 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Dialog
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
@@ -153,4 +156,20 @@ class MainActivity : AppCompatActivity() {
     }
     builder.create().show()
   }
+
+  private fun getBitmapFromView(view: View): Bitmap {
+    val returnedBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(returnedBitmap)
+    val bgDrawable = view.background
+    if(bgDrawable != null){
+      bgDrawable.draw(canvas)
+    }else{
+      canvas.drawColor(Color.WHITE)
+    }
+
+    view.draw(canvas)
+
+    return returnedBitmap
+  }
+
 }
